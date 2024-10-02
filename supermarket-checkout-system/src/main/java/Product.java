cdc14f0d45778a6c2ec669d25bad03bb70fe2d1a
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-10-02 14:30:33 +0100

    updated Product class, added Constructors

--- supermarket-checkout-system/src/main/java/Product.java
+++ supermarket-checkout-system/src/main/java/Product.java
@@ -1,10 +1,8 @@
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Product {
 
     private String sku;
@@ -12,4 +10,15 @@ public class Product {
     private int specialQuantity;
     private int specialPrice;
 
+    public Product(String sku, int unitPrice, int specialQuantity, int specialPrice) {
+        this.sku = sku;
+        this.unitPrice = unitPrice;
+        this.specialQuantity = specialQuantity;
+        this.specialPrice = specialPrice;
+    }
+
+    public Product(String sku, int unitPrice) {
+        this.sku = sku;
+        this.unitPrice = unitPrice;
+    }
 }
