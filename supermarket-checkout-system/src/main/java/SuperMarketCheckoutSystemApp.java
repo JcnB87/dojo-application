e9b6cadf255fcd49baf32c6e44de198f59c11c23
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-10-02 14:30:01 +0100

    added rest of sku items SuperMarketCheckoutSystemApp

--- supermarket-checkout-system/src/main/java/SuperMarketCheckoutSystemApp.java
+++ supermarket-checkout-system/src/main/java/SuperMarketCheckoutSystemApp.java
@@ -6,21 +6,43 @@ public class SuperMarketCheckoutSystemApp {
 
     public static void main(String[] args) {
         System.out.println("Welcome!!! Dear Customer attention our store has deals in place! please pick your products/items! Thank you");
-        Product itemA = new Product("A", 50,3,130);
 
-        //Defining pricing rules
+        //Create new Product instances, product deals.
+        Product itemA = new Product("A", 50, 3, 130);
+        Product itemB = new Product("B", 30, 2, 45);
+        Product itemC = new Product("C", 20);
+        Product itemD = new Product("D", 15);
+
+        //Adding products to the basket
         Map<String, Product> pricingRules = new HashMap<>();
         pricingRules.put("A", itemA);
+        pricingRules.put("B", itemB);
+        pricingRules.put("C", itemC);
+        pricingRules.put("D", itemD);
 
-        //Create checkout system and Scanner for inputs in console
+        //Create checkout system
         Checkout checkout = new Checkout(pricingRules);
+
+        //Scanner for console inputs
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter SKU codes to scan: ");
+        System.out.println("Enter SKU codes to scan (or enter 'total' to calculate the total): ");
 
-        scanner.nextLine();
+        while (true) {
+            String input = scanner.nextLine().trim();
 
+            if (input.equalsIgnoreCase("total")) {
+                int total = checkout.calculateTotal();
+                System.out.println("Total price is: " + total + " pence");
+                break;
+            } else {
+                try {
+                    checkout.scanItems(input);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
         scanner.close();
-
-
+        System.out.println("Thank you for shopping at our store!");
     }
 }
