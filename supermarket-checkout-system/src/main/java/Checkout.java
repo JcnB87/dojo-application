955958d84742a4a484b73be4e4d9d1991beab1a8
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-10-02 14:31:49 +0100

    updated Checkout class, updated Constructors and fixed an issue regarding ´scannedItems´ null pointer

--- supermarket-checkout-system/src/main/java/Checkout.java
+++ supermarket-checkout-system/src/main/java/Checkout.java
@@ -1,15 +1,36 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Checkout {
 
-    private Map<String, Product> priceRules;
+    private Map<String, Product> pricedRules;
     private Map<String, Integer> scannedItems;
 
-    public Checkout(Map<String,Product> pricingRules) {
+    public Checkout(Map<String, Product> pricedRules) {
+        this.pricedRules = pricedRules;
+        this.scannedItems = new HashMap<>();
     }
 
-    public Checkout(Map<String,Product> priceRules, Map<String, Integer> scannedItems) {
-        this.priceRules = priceRules;
+    public Checkout(Map<String, Product> pricedRules, Map<String, Integer> scannedItems) {
+        this.pricedRules = pricedRules;
         this.scannedItems = scannedItems;
+    }
+
+    /**
+     * @param sku receives the argument from console input. i.e: 'A'.
+     *            This method will check if sku 'A' exists in our list of items (if key 'A' exists from our map 'pricedRules')
+     *            then increment by 1.
+     *            If not, will throw an IllegalArgumentException with the message.
+     */
+    public void scanItems(String sku) {
+        if (!pricedRules.containsKey(sku)) {
+            throw new IllegalArgumentException("Invalid SKU: " + sku);
+        }
+        scannedItems.put(sku, scannedItems.getOrDefault(sku, 0) + 1);
+        System.out.println("Scanned item: " + sku);
+    }
+
+    public int calculateTotal() {
+        return 0;
     }
 }
