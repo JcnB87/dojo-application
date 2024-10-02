521f6749232ad6a322b0129b92d07e40bfe3c66b
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-10-02 12:16:59 +0100

    updated SuperMarketCheckoutSystemApp class: initialized app entry point, created Product and Checkout instances and added Scanner instance to check/test if working

--- supermarket-checkout-system/src/main/java/SuperMarketCheckoutSystemApp.java
+++ supermarket-checkout-system/src/main/java/SuperMarketCheckoutSystemApp.java
@@ -1,6 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class SuperMarketCheckoutSystemApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome!!!");
+        System.out.println("Welcome!!! Dear Customer attention our store has deals in place! please pick your products/items! Thank you");
+        Product itemA = new Product("A", 50,3,130);
+
+        //Defining pricing rules
+        Map<String, Product> pricingRules = new HashMap<>();
+        pricingRules.put("A", itemA);
+
+        //Create checkout system and Scanner for inputs in console
+        Checkout checkout = new Checkout(pricingRules);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter SKU codes to scan: ");
+
+        scanner.nextLine();
+
+        scanner.close();
+
+
     }
 }
